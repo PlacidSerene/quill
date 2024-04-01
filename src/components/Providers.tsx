20cda5dd60f62ddb96de2a84f3b090e9e6a8a5d7
Daniel <t.dung.lhp@gmail.com>
2024-04-01 21:40:32 +0700

    hardcode the provider url

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -21,7 +21,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `${process.env.VERCEL_URL}/api/trpc`,
+          url: "https://quill-nu-blue.vercel.app/api/trpc",
         }),
       ],
     })
